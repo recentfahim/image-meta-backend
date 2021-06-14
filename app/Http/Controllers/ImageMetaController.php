838b0727d2b4dd932e4e782ee0dc1c145332ab18
recentfahim <fahim@techcare.co>
2021-06-14 21:34:37 +0600

    added exif and iptc data in api response

--- app/Http/Controllers/ImageMetaController.php
+++ app/Http/Controllers/ImageMetaController.php
@@ -10,8 +10,10 @@ class ImageMetaController extends Controller
 {
     public function ExtractImageMeta(Request $request){
         if($request->image){
-            $data = Image::make($request->image)->exif();
-            Log::channel('stderr')->info($data);
+            $exif_info = Image::make($request->image)->exif();
+            Log::channel('stderr')->info($request->all());
+            $iptc = Image::make($request->image)->iptc();
+            return response()->json(array('result' => array('exif_data' => $exif_info, 'iptc_data' => $iptc), 'success' => true), 200);
         }
     }
 }
