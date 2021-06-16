313702ca822e2c8e07bb884371fbbb84fac70964
recentfahim <fahim@techcare.co>
2021-06-16 22:21:37 +0600

    added camera info from exif and remove some field

--- app/Http/Controllers/ImageMetaController.php
+++ app/Http/Controllers/ImageMetaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Intervention\Image\ImageManagerStatic as Image;
@@ -11,9 +12,28 @@ class ImageMetaController extends Controller
     public function ExtractImageMeta(Request $request){
         if($request->image){
             $exif_info = Image::make($request->image)->exif();
-            Log::channel('stderr')->info($request->all());
             $iptc = Image::make($request->image)->iptc();
-            return response()->json(array('result' => array('exif_data' => $exif_info, 'iptc_data' => $iptc), 'success' => true), 200);
+            unset($exif_info['COMPUTED']);
+            unset($exif_info['THUMBNAIL']);
+            $exif_info['FileName'] = $request->image->getClientOriginalName();
+            $camera_info = null;
+            try {
+                $camera_info = array(
+                    "Make" => $exif_info['Make'],
+                    "Model" => $exif_info['Model'],
+                    "Exposure" =>$exif_info['ExposureTime'],
+                    "Aperture" => $exif_info['FNumber'],
+                    "Focal Length" =>$exif_info['FocalLength'],
+                    "ISO Speed" =>$exif_info['ISOSpeedRatings'],
+                    "Flash" =>$exif_info['Flash']
+                );
+            }
+            catch(Exception $e){
+                return response()->json(array('result' => array('exif_data' => $exif_info, 'iptc_data' => $iptc, 'camera_info' => $camera_info), 'success' => true), 200);
+            }
+
+            return response()->json(array('result' => array('exif_data' => $exif_info, 'iptc_data' => $iptc, 'camera_info' => $camera_info), 'success' => true), 200);
+
         }
     }
 }
