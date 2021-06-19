56a5b94ce58593624582806f0527bfb41af1ca67
recentfahim <fahim@techcare.co>
2021-06-20 02:16:00 +0600

    cors remove

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,8 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->register(Fruitcake\Cors\CorsServiceProvider::class);
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
